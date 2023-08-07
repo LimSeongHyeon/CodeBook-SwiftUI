d8883fb377d8ede2c82f40a4b87423f8c94a3a58
LimSeongHyeon <shot7351@gmail.com>
2023-08-07 09:01:05 +0900

    Initial Commit

---
+++ CodeBook/CodeBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBookApp.swift
+//  CodeBook
+//
+//  Created by TommyFuture on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct CodeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
